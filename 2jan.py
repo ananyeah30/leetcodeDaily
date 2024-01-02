d43e826cab834bf0e2157095a3c390ca1c5bed4d
Ananya Choudhary <97330172+ananyeah30@users.noreply.github.com>
2024-01-02 23:16:36 +0530

    Create 2jan.py

---
+++ 2jan.py
@@ -0,0 +1,11 @@
+Valid Palindrome :https://leetcode.com/problems/valid-palindrome/
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        lowercase=s.lower()
+        my_list=[]
+        for char in lowercase:
+            if char.isalnum():
+                my_list.append(''.join(char))
+        print()
+        return my_list==my_list[::-1]
