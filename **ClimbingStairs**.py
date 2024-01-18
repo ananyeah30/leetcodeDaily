bfbb4fe9828d353cc25431cec9d7a610ec981005
Ananya Choudhary <97330172+ananyeah30@users.noreply.github.com>
2024-01-18 23:56:31 +0530

    Create **ClimbingStairs**.py

---
+++ **ClimbingStairs**.py
@@ -0,0 +1,16 @@
+https://leetcode.com/problems/climbing-stairs/submissions/1150032110/?envType=daily-question&envId=2024-01-18
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        ways = 1
+
+        for i in range(1, (n // 2) + 1):
+            product = 1
+
+            for j in range(i, 2 * i):
+                product *= (n - j) / (j - i + 1)
+
+            ways += product
+
+        return int(ways)
